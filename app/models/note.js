95ee6cc616fe5118a30c8b9a46aba9e7fda796f7
ashwinkumar-sg <ashwin.goudars@gmail.com>
2021-07-16 17:44:36 +0530

    first commit

---
+++ app/models/note.js
@@ -0,0 +1,42 @@
+ const mongoose = require('mongoose')
+ //npm install validate
+
+// schema
+const Schema = mongoose.Schema
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true, 
+        // custom validation
+        // validate: {
+        //     validator: function(value){
+        //        return   condition ? true : false 
+        //     }, 
+        //     message: function(){
+        //         return 'something went wrong'
+        //     }
+        // }
+    }, 
+    body: {
+        type: String,
+        required: true 
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Category'
+    }
+})
+
+// mongoose middleware
+// pre('validate')
+noteSchema.pre('save', function(next){
+    console.log('function called before saving a record')
+    next()
+})
+
+
+// model 
+const Note = mongoose.model('Note', noteSchema)
+
+module.exports = Note
---
+++ config/routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const notesController = require('../app/controllers/notesController') 
+const categoriesController = require('../app/controllers/categoriesController')
+
+console.log(categoriesController)
+ 
+router.get('/notes', notesController.list)
+router.get('/notes/:id', notesController.show)
+router.post('/notes', notesController.create)
+router.put('/notes/:id', notesController.update)
+router.delete('/notes/:id', notesController.destroy)
+
+router.get('/categories', categoriesController.list)
+router.post('/categories', categoriesController.create)
+router.get('/categories/:id', categoriesController.show)
+
+module.exports = router
+
+    
